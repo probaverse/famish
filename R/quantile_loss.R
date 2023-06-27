b5b44a2f605ee964d2506c9b3433ed00e11ce9e2
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-06-27 09:38:07 -0700

    update quantile score nomenclature and functionality

--- R/quantile_loss.R
+++ R/quantile_loss.R
@@ -1,15 +1,26 @@
+#' Quantile score function
+#'
+#' Evaluates the quantile score for a given observation against a quantile
+#' estimate.
+#'
+#' @param x Vector of observed data values.
+#' @param xhat Vector of estimated quantiles.
+#' @param tau Quantile level (non-exceedance probability).
+#' @return The evaluated quantile score for each entry in the input vector(s).
+#' @note The quantile score is based on a loss function that goes by many
+#' names -- the "asymmetric absolute
+#' deviation function", the "stick function", the "check function",
+#' or the "pinball loss".
+#' @export
+quantile_score <- function(x, xhat, tau) {
+  z <- vctrs::vec_recycle_common(x, xhat, tau)
+  stick_function(z[[1]] - z[[2]], z[[3]])
+}
+
 #' Quantile loss function
 #'
-#' Evaluates the quantile loss function.
-#'
-#' @param x Vector of observed values to evaluate.
-#' @param tau Quantile level. Vectorized.
-#' @return The evaluated loss function for each entry in the input vector(s).
-#' @note This function is sometimes also called the "asymmetric absolute
-#' deviation function", or the "stick function", or the "check function".
-#'
-#' This is a lightweight function that does not check its inputs.
-#' @export
-quantile_loss <- function(x, tau) {
+#' @param x Vector of values to evaluate the loss function at.
+#' @param tau Vector of non-exceedance probabilities.
+stick_function <- function(x, tau) {
   (tau - (x < 0)) * x
 }
