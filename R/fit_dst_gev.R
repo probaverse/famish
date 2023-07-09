88f99c6fedfd46c7e8bf7fd9a6fffdda3232ec88
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-07-09 14:43:17 -0700

    add GPD fit

--- R/fit_dst_gev.R
+++ R/fit_dst_gev.R
@@ -1,23 +1,38 @@
-#' Fit GEV distribution
+#' Fit GPD distribution
 #'
 #' @inheritParams fit_dst_norm
 #' @param diagnostics Logical; print out diagnostic plots of the fit?
+#' @param threshold Single numeric indicating the left-endpoint of the
+#' distribution.
 #' @return A distplyr distribution.
 #' @export
 fit_dst_gev <- function(x, method = c("mle", "lmom", "mom", "mge"),
-                        diagnostics = FALSE, ...) {
+                        diagnostics = FALSE, threshold, ...) {
   method <- rlang::arg_match(method)
   if (method == "mle") {
-    fit_ismev <- ismev::gev.fit(x, show = FALSE)
-    if (diagnostics) {
-      ismev::gev.diag(fit_ismev)
+    if (missing(threshold)) {
+      stop("`threshold` parameter is required when fitting via MLE.")
     }
-    return(distionary::dst_gev(fit_ismev$mle[1], fit_ismev$mle[2],
-                               fit_ismev$mle[3]))
+    m <- min(x, na.rm = TRUE)
+    if (threshold > m) {
+      stop(glue("There are data below the specified GPD threshold of ",
+                "{threshold}. Either set the threshold at {m} or below, ",
+                "or truncate the data below {threshold}."))
+    }
+    fit_ismev <- ismev::gpd.fit(x, threshold = threshold, show = FALSE)
+    if (diagnostics) {
+      ismev::gpd.diag(fit_ismev)
+    }
+    return(distionary::dst_gpd(threshold, fit_ismev$mle[1], fit_ismev$mle[2]))
   }
   if (method == "lmom") {
+    if (!missing(threshold)) {
+      stop("Sorry, but the current implementation of l-moments requires the ",
+           "threshold to not be specified (it will be estimated).")
+    }
     params <- lmom::pelgev(lmom::samlmu(x))
-    return(distionary::dst_gev(params[[1]], params[[2]], -params[[3]]))
+    xi <- -params[[3]]
+    return(distionary::dst_gev(params[[1]], params[[2]], xi))
   }
   stop("That method has not been implemented yet.")
 }
